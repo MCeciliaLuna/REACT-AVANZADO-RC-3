888fffdada3f9f4712ce3cdfb1086e56dc35cdc8
MCeciliaLuna <lunama.cecilia@live.com>
2024-01-10 19:41:48 -0300

    usememo in categories

--- src/components/filters/CustomSelect.jsx
+++ src/components/filters/CustomSelect.jsx
@@ -1,15 +1,26 @@
+import { useMemo } from "react";
+
 export const CustomSelect = ({ onChangeCategories, index, categories }) => {
+  const getOptions = () => {
+    return categories.map((item) => (
+      <option defaultValue={true} key={index} value={item.name}>
+        {item.name}
+      </option>
+    ));
+  };
+
+  const options = useMemo(() => 
+  {
+    return getOptions()
+  }, [onChangeCategories])
+
   return (
     <select
       className="form-select w-75"
       aria-label="Default select example"
       onChange={onChangeCategories}
     >
-      {categories.map((item) => (
-        <option defaultValue={true} key={index} value={item.name}>
-          {item.name}
-        </option>
-      ))}
+      {options}
     </select>
   );
 };
